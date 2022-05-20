e3d1ba504b78379fb3a4e36ee208672c1e4f2910
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-05-20 17:51:09 +0200

    Setup iniziale.

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+  width: 100%;
+  height: 90px;
+  background-color: black;
+}
+</style>
